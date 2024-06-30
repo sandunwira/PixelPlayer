d263c62022121f54c7caa1f1c4d6172a8c3553bc
sandunwira <79461263+sandunwira@users.noreply.github.com>
2024-06-30 13:41:22 +0530

    Basic UI

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { appWindow } = window.__TAURI__.window;
+
+const images = document.getElementsByTagName('img');
+const refreshBtn = document.getElementById('refreshBtn');
+
+
+document.getElementById('titlebar-minimize').addEventListener('click', () => appWindow.minimize());
+document.getElementById('titlebar-maximize').addEventListener('click', () => appWindow.toggleMaximize());
+document.getElementById('titlebar-close').addEventListener('click', () => appWindow.close());
+
+
+for (let i = 0; i < images.length; i++) {
+	images[i].addEventListener('mousedown', function (e) {
+		e.preventDefault();
+	});
+}
+
+
+document.addEventListener('contextmenu', function (e) {
+	e.preventDefault();
+});
+
+
+refreshBtn.addEventListener('click', () => {
+	window.location.reload();
+});
